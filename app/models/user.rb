19b9a631f18e4d1ebcd4da0eed86d4e8cd9fec47
YoniA <YoniA@cat.com>
2021-01-22 15:33:15 +0200

    add password validations: min length and cannot be blank. write appropriate tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,6 @@ class User < ApplicationRecord
                         uniqueness: true
 
     has_secure_password
+    validates :password, presence: true, length: {minimum: 6}
                     
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class UserTest < ActiveSupport::TestCase
   
   def setup
-    @user = User.new(name: "Example user", email: "example@user.com")
+    @user = User.new(name: "Example user", email: "example@user.com", password: "foobar", password_confirmation: "foobar")
   end
 
   test "should be valid" do
@@ -55,5 +55,15 @@ class UserTest < ActiveSupport::TestCase
     @user.email = mixed_case_email
     @user.save
     assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
+  test "password should not be blank" do
+    @user.password = @user.password_confirmation =  " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimal length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
   end
 end
