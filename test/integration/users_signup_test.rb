cebd81861c4f2f96f68ca632f2427bdf957b808f
YoniA <YoniA@cat.com>
2021-02-20 17:53:14 +0200

    add integration test for successful signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -15,4 +15,19 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_template 'users/new' # re-render users#new upon failed submission
     assert_select 'div#error-explanation'
   end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: {user: { name: "Example User",
+                                        email: "foo@bar.com",
+                                        password: "foobar",
+                                        password_confirmation: "foobar"
+                                      }
+                                }
+        
+    end
+    follow_redirect!
+    assert_template 'users/show' # re-render users#new upon failed submission
+  end
 end
